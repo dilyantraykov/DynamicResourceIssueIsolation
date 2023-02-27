a8b079238dde40de63558a2f0f454b9a5fc84234
Dilyan Traykov <dtraykov94@gmail.com>
2023-02-27 16:09:33 +0200

    Remove unnecessary references and namespaces.

--- ButtonTests/MauiProgram.cs
+++ ButtonTests/MauiProgram.cs
@@ -1,6 +1,4 @@
-﻿using Telerik.Maui.Controls.Compatibility;
-
-namespace ButtonTests;
+﻿namespace ButtonTests;
 
 public static class MauiProgram
 {
@@ -9,7 +7,6 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-            .UseTelerik()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
