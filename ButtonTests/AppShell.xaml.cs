d615627af36fb3d6800f01715755465a1f043168
Dilyan Traykov <dtraykov94@gmail.com>
2023-02-27 15:55:21 +0200

    Add isolation project

---
+++ ButtonTests/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ButtonTests;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ButtonTests/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace ButtonTests;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+    }
+
+    private void Start_OnClicked(object sender, EventArgs e)
+	{
+        StartBtn.IsEnabled = StartBtnDynamic.IsEnabled = false;
+        StopBtn.IsEnabled = StopBtnDynamic.IsEnabled = true;
+    }
+}
+
